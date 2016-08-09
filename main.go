73a745bd3fa15505f4cd8a70ce1e8c04bb8ef92d
Colin Walker <colin@musefind.com>
2016-08-08 20:56:34 -0700

    remove cli packange

--- main.go
+++ main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"github.com/urfave/cli"
 	. "github.com/coldog/scheduler/api"
-	. "github.com/coldog/scheduler/cli"
 	. "github.com/coldog/scheduler/agent"
 	. "github.com/coldog/scheduler/scheduler"
 
 	"os"
 	"fmt"
+	"io/ioutil"
+	"github.com/ghodss/yaml"
+	"github.com/syndtr/goleveldb/leveldb/errors"
 )
+
+var ValidationErr error = errors.New("Validation Failed")
+
+func valid(errs []string) error {
+	if len(errs) > 0 {
+		for _, err := range errs {
+			fmt.Printf("err: %s\n", err)
+		}
+		return ValidationErr
+	}
+	return nil
+}
+
+func readYml(file string, res interface{}) error {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	err = yaml.Unmarshal([]byte(data), res)
+	return err
+}
+
+func printOk() {
+	fmt.Println("OK")
+}
 
 func main() {
 	api := NewSchedulerApi()
@@ -20,11 +48,64 @@ func main() {
 	app.Author = "Colin Walker"
 	app.Usage = "schedule tasks across a consul cluster."
 
-	RegisterClusterCommands(api, app)
-	RegisterTaskCommands(api, app)
-	RegisterTaskDefinitionCommands(api, app)
-	RegisterServiceCommands(api, app)
-	RegisterConfigCommands(api, app)
+	app.Commands = append(app.Commands, cli.Command{
+		Name: "schedule",
+		Usage: "trigger the scheduler",
+		Action: func(c *cli.Context) error {
+			api.TriggerScheduler()
+			printOk()
+			return nil
+		},
+	})
+
+	app.Commands = append(app.Commands, cli.Command{
+		Name: "apply",
+		Usage: "apply a configuration file to the cluster",
+		Action: func(c *cli.Context) error {
+			obj := struct {
+				Clusters []Cluster `json:"clusters"`
+				Services []Service `json:"services"`
+				Tasks []TaskDefinition `json:"tasks"`
+			}{}
+
+			err := readYml(c.Args().First(), &obj)
+			if err != nil {
+				return err
+			}
+
+			for _, task := range obj.Tasks {
+				err = valid(task.Validate(api))
+				if err == nil {
+					api.PutTaskDefinition(task)
+					fmt.Printf("task: OK %s\n", task.Name)
+				} else {
+					fmt.Printf("task: FAIL %s\n", task.Name)
+				}
+			}
+
+			for _, service := range obj.Services {
+				err = valid(service.Validate(api))
+				if err == nil {
+					api.PutService(service)
+					fmt.Printf("service: OK %s\n", service.Name)
+				} else {
+					fmt.Printf("service: FAIL %s\n", service.Name)
+				}
+			}
+
+			for _, cluster := range obj.Clusters {
+				err = valid(cluster.Validate(api))
+				if err == nil {
+					api.PutCluster(cluster)
+					fmt.Printf("cluster: OK %s\n", cluster.Name)
+				} else {
+					fmt.Printf("cluster: FAIL %s\n", cluster.Name)
+				}
+			}
+
+			return nil
+		},
+	})
 
 	app.Commands = append(app.Commands, cli.Command{
 		Name: "combined",
